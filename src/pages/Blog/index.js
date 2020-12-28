2fbf2bc9212caa402261badc441283805bca0754
Mariana <moraismariana83@gmail.com>
2020-12-28 00:52:03 -0300

    Fixing smt

--- src/pages/Blog/index.js
+++ src/pages/Blog/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import Header from '../../components/Header/NavBar';
 
 export default function Home() {
   return (
     <>
-      <Header />
       <h1>Blog</h1>
     </>
   );
