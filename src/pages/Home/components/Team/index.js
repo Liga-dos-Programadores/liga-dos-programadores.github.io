08ad0c3638c3f1ab0fc1c7b15d015fe17ebf2f33
miguelrisquelme <miguelrisquelme@gmail.com>
2021-03-17 17:54:11 -0300

    Feito parte de nossa equipe

--- src/pages/Home/components/Team/index.js
+++ src/pages/Home/components/Team/index.js
@@ -1,30 +1,97 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+import {
+  ContentTeam,
+  TilteTeam,
+  Teamates,
+  Teammate,
+  TeammateImage,
+  TeammateName,
+  TeammateAbout,
+  TeammateUsername,
+  IconsGroup,
+  GithubIcon,
+  LinkA,
+  WebsiteIcon,
+  TwitterIcon
+} from './styled';
+
+// import { AiFillGithub } from 'react-icons/ai';
+
 export default function Team() {
-  const [data, setData] = useState('');
+  // eslint-disable-next-line prefer-const
+  const [infoTeam, setInfoTeam] = useState([]);
 
   useEffect(() => {
     axios({
       method: 'get',
       url: 'https://api.github.com/orgs/Liga-dos-Programadores/members'
-    })
-      .then((response) => {
-        console.log(response.data);
-        setData(response.data);
+    }).then((response) => {
+      Promise.all(
+        response.data.map(
+          ({ url }) =>
+            axios({
+              method: 'get',
+              url
+            }).then(({ data }) => ({
+              name: data.name,
+              avatar: data.avatar_url,
+              website: data.blog,
+              twitter: data.twitter_username,
+              email: data.email,
+              username: data.login
+            }))
+          // eslint-disable-next-line function-paren-newline
+        )
+      ).then((teammates) => {
+        console.log(teammates);
+        setInfoTeam(teammates);
       });
+    });
   }, []);
 
   return (
-    <div>
-      <h1>Equipe</h1>
-      {data.map((index) => (
-        <div>
-          {index.login}
-          <img src={index.avatar_url} alt="Avatar" />
-          {index.html_url}
-        </div>
-      ))}
-    </div>
+    <ContentTeam>
+      <TilteTeam>Nossa Equipe</TilteTeam>
+      <Teamates>
+        {infoTeam.map((index) => (
+          <Teammate>
+            <TeammateImage src={index.avatar} alt={index.username} />
+            <TeammateName>
+              {index.name}
+              <TeammateUsername>{index.username}</TeammateUsername>
+            </TeammateName>
+            <IconsGroup>
+              <LinkA
+                href={`https://github.com/${index.username}`}
+                target="_blank"
+              >
+                <GithubIcon />
+              </LinkA>
+
+              {index.website ? (
+                <LinkA href={index.website} target="_blank">
+                  <WebsiteIcon />
+                </LinkA>
+              ) : (
+                <></>
+              )}
+
+              {index.twitter ? (
+                <LinkA
+                  href={`https://twitter.com/${index.twitter}`}
+                  target="_blank"
+                >
+                  <TwitterIcon />
+                </LinkA>
+              ) : (
+                <></>
+              )}
+            </IconsGroup>
+          </Teammate>
+        ))}
+      </Teamates>
+    </ContentTeam>
   );
 }
--- src/pages/Home/components/Team/styled.js
+++ src/pages/Home/components/Team/styled.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
+import { AiFillGithub as IGithub } from 'react-icons/ai';
+import { MdWebAsset as IWebsite } from 'react-icons/md';
+import { FaTwitter as ITwitter } from 'react-icons/fa';
+
 export const TeamTitle = styled.h2`
   text-align: center;
   font-size: 30px;
@@ -14,4 +18,108 @@ export const TeamContainer = styled.div`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
+`;
+
+export const ContentTeam = styled.div``;
+
+export const TilteTeam = styled.h2`
+  text-align: center;
+  font-size: 30px;
+  font-weight: 600;
+  margin-top: 100px;
+  color: var(--color-white);
+`;
+
+export const Teamates = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 38%);
+`;
+
+export const Teammate = styled.div`
+  height: 450px;
+  width: 300px;
+  margin-top: 100px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+  border-radius: 5px;
+  background-color: var(--color-card-team);
+`;
+
+export const TeammateImage = styled.img`
+  width: 200px;
+  border-radius: 100px;
+`;
+
+export const TeammateName = styled.p`
+  font-weight: bold;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const TeammateAbout = styled.button`
+  font-size: 21px;
+  font-weight: 500;
+  color: var(--color-white);
+  background-color: var(--color-blue);
+  margin: 19px 0 0;
+  padding: 16px 35px;
+  text-decoration: none;
+  border-radius: 5px;
+  transition: all 150ms linear 0s;
+  text-align: center;
+  :hover {
+    background-color: var(--color-light-blue);
+    cursor: pointer;
+  }
+
+  @media (max-width: 768px) {
+    width: 82%;
+  }
+`;
+
+export const TeammateUsername = styled.span`
+  font-weight: 300;
+  font-size: 12px;
+  margin-top: 2px;
+`;
+
+export const IconsGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 80%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const GithubIcon = styled(IGithub)`
+  width: 40px;
+  height: 40px;
+  color: #fff;
+  margin: 0px 5px;
+`;
+
+export const WebsiteIcon = styled(IWebsite)`
+  width: 40px;
+  height: 40px;
+  color: #fff;
+  margin: 0px 5px;
+`;
+
+export const TwitterIcon = styled(ITwitter)`
+  width: 40px;
+  height: 40px;
+  color: #fff;
+  margin: 0px 5px;
+`;
+
+export const LinkA = styled.a`
+  opacity: 0.85;
+  &:hover {
+    opacity: 1;
+  }
 `;
